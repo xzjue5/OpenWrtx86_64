825f8c7c2dfe2b81b007d1c8f638487b5bbb8318
xzjue5 <40517748+xzjue5@users.noreply.github.com>
2023-01-31 18:13:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,21 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# KoolProxyR去广告插件
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
-# 京东签到插件
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
-# adguardhome插件
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-# Clash插件
-git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-# SmartDNS插件
-git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-
-# argon主题
-pushd package/lean
-rm -rf luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
-popd
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
